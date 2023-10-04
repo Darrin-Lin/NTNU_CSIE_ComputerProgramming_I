23b07b7905cc1a31dbae9f98ed9404f8dd1a1421
Darrin-Lin <darrin.de88@gmail.com>
2023-10-04 18:12:48 +0800

    add Hoomework2

---
+++ Homework2/test_hw02/test0204.py
@@ -0,0 +1,7 @@
+import random as r
+hw=2
+hw_part=4
+for i in range(0,10):
+    with open('./0'+str(hw_part)+'/in/hw0'+str(hw)+'0'+str(hw_part)+'-'+str(i)+'.in', 'w') as f:
+        f.write()
+        f.write('\n')
