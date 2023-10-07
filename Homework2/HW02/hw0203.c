3cc20f12d60cfe32c8abbcbd46942afce9f782bb
Darrin-Lin <darrin.de88@gmail.com>
2023-10-07 16:17:06 +0800

    hw0203 finish

--- Homework2/HW02/hw0203.c
+++ Homework2/HW02/hw0203.c
@@ -1,30 +1,172 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// int32_t S0(int32_t n)
-// {
-// }
-
 int main()
 {
     int32_t n = 0;
-    int8_t S0 = 1, S1 = 0, S2 = 0, S3 = 0, S4 = 0, S5 = 0, S6 = 0;
-    do{
+    int8_t run_S0 = 0, run_S1 = 0, run_S2 = 0, run_S3 = 0, run_S4 = 0, run_S5 = 0, run_S6 = 0;
+    int8_t now_S0 = 1, now_S1 = 0, now_S2 = 0, now_S3 = 0, now_S4 = 0, now_S5 = 0, now_S6 = 0;
+    int8_t not_first_print = 0;
+    do
+    {
+        printf("Please enter an integer: ");
         scanf("%d", &n);
-        if (!n)
+        if (n == 0)
             break;
-        if (S0)
+        // which run
+        if (now_S0)
         {
-            S0=1;
-            if (n %2)///odd
+            now_S0 = 0;
+            if (n % 2) /// Odd
             {
-                S1=1;//fix next S
-                S2=1;
+                run_S1 = 1;
+                run_S2 = 1;
+            }
+            else // Even
+                run_S3 = 1;
+        }
+        if (now_S1)
+        {
+            now_S1 = 0;
+            if (n % 2) /// Odd
+                run_S2 = 1;
+            else // Even
+                run_S4 = 1;
+        }
+        if (now_S2)
+        {
+            now_S2 = 0;
+            if (n % 2) /// Odd
+                run_S3 = 1;
+            else // Even
+                run_S5 = 1;
+        }
+        if (now_S3)
+        {
+            now_S3 = 0;
+            if (n % 2) /// Odd
+                run_S5 = 1;
+            else // Even
+                run_S0 = 1;
+        }
+        if (now_S4)
+        {
+            now_S4 = 0;
+            if (n % 2) /// Odd
+                run_S5 = 1;
+            else // Even
+            {
+                run_S2 = 1;
+                run_S6 = 1;
             }
         }
+        if (now_S5)
+        {
+            now_S5 = 0;
+            if (n % 2) /// Odd
+                run_S6 = 1;
+            else // Even
+                run_S0 = 1;
+        }
+        if (now_S6)
+        {
+            now_S6 = 0;
+            if (n % 2) /// Odd
+                run_S6 = 1;
+            else // Even
+                run_S1 = 1;
+        }
+        // add now_Sn
+        if (run_S0)
+        {
+            now_S0 = 1;
+            run_S0 = 0;
+        }
+        if (run_S1)
+        {
+            now_S1 = 1;
+            run_S1 = 0;
+        }
+        if (run_S2)
+        {
+            now_S2 = 1;
+            run_S2 = 0;
+        }
+        if (run_S3)
+        {
+            now_S3 = 1;
+            run_S3 = 0;
+        }
+        if (run_S4)
+        {
+            now_S4 = 1;
+            run_S4 = 0;
+        }
+        if (run_S5)
+        {
+            now_S5 = 1;
+            run_S5 = 0;
+        }
+        if (run_S6)
+        {
+            now_S6 = 1;
+            run_S6 = 0;
+        }
+    } while (n);
+    printf("Possible States: ");
+    if (now_S0)
+    {
+        not_first_print = 1;
+        printf("S0");
     }
-    while (n)
-        ;
-
+    if (now_S1)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S1");
+    }
+    if (now_S2)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S2");
+    }
+    if (now_S3)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S3");
+    }
+    if (now_S4)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S4");
+    }
+    if (now_S5)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S5");
+    }
+    if (now_S6)
+    {
+        if (not_first_print)
+            printf(", ");
+        else
+            not_first_print = 1;
+        printf("S6");
+    }
+    printf("\n");
     return 0;
 }
--- README.md
+++ README.md
@@ -32,3 +32,4 @@ Start to write hw0201 and finished.
 
 ### 10/7 Update
 Start to write hw0204 and finished.
+Start to write hw0203 and finished.
