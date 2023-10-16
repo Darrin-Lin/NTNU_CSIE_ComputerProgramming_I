cfc044a0e965cad49c76e0e515ec00b25013eb1e
Darrin-Lin <darrin.de88@gmail.com>
2023-10-16 16:17:41 +0800

    add compare shell script

---
+++ test_compare/compair.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "hw number:"
+read hw
+#hw=
+echo part number:
+read n
+echo "directory name:"
+read directory
+echo "file name:"
+read filename
+
+for i in $(seq 0 9); do
+    diff ./0${n}/out/hw0${hw}0${n}_0${i}.out ./directory/filename0${hw}0${n}_0${i}.out
+done
+for i in $(seq 10 19); do
+    if [ -f "./0${n}/in/hw0${hw}0${n}_${i}.out" ]; then
+        diff ./0${n}/out/hw0${hw}0${n}_${i}.out ./directory/filename0${hw}0${n}_${i}.out
+    fi
+done
