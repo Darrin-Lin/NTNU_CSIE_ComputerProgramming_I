67cfc691a7dcd35c2cc9d6cb29fa0196760bf64d
Darrin-Lin <darrin.de88@gmail.com>
2023-11-20 13:42:47 +0800

    update test0403

--- Homework4/test_hw04/test0403.py
+++ Homework4/test_hw04/test0403.py
@@ -61,7 +61,7 @@ for i in range(0,1):
         f.write("};")
         f.write('\n')
 
-for i in range(1,5):
+for i in range(1,4):
     with open('./0'+str(hw_part)+'/in/hw0'+str(hw)+'0'+str(hw_part)+"_0"+str(i)+'.in', 'w') as f:
         f.write("#include <stdint.h>\n")
         f.write("#pragma once\n")
@@ -80,6 +80,66 @@ for i in range(1,5):
             f.write("{")
             for j in range(0,6):
                 f.write(str(r.randint(0,15)))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        f.write("};")
+        f.write('\n')
+for i in range(4,5):
+    with open('./0'+str(hw_part)+'/in/hw0'+str(hw)+'0'+str(hw_part)+"_0"+str(i)+'.in', 'w') as f:
+        f.write("#include <stdint.h>\n")
+        f.write("#pragma once\n")
+        n=100
+        num=[0,1,2,3,4,5]
+        r.shuffle(num)
+        f.write("#define STUDENT_NUMBER ("+str(n)+")\n")
+        f.write("#define CHINESE ("+str(num[0])+")\n")
+        f.write("#define ENGLISH ("+str(num[1])+")\n")
+        f.write("#define MATH_A ("+str(num[2])+")\n")
+        f.write("#define MATH_B ("+str(num[3])+")\n")
+        f.write("#define SOCIAL ("+str(num[4])+")\n")
+        f.write("#define SCIENCE ("+str(num[5])+")\n")
+        f.write("int32_t score[][6]=")
+        f.write("{")
+        for k in range(0,11):
+            f.write("{")
+            for j in range(0,6):
+                f.write(str(j+1))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        for k in range(11,24):
+            f.write("{")
+            for j in range(0,6):
+                f.write(str(j+3))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        for k in range(24,49):
+            f.write("{")
+            for j in range(0,6):
+                f.write(str(j+5))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        for k in range(49,74):
+            f.write("{")
+            for j in range(0,6):
+                f.write(str(j+7))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        for k in range(74,87):
+            f.write("{")
+            for j in range(0,6):
+                f.write(str(j+9))
+                if j!=5:
+                    f.write(",")
+            f.write("},\n")
+        for k in range(87,100):
+            f.write("{")
+            for j in range(0,6):
+                f.write("15")
                 if j!=5:
                     f.write(",")
             f.write("},\n")
