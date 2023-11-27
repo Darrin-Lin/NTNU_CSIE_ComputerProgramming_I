f6cd32b86d9684d8e47251a007a93f3c9bdbd5fe
Darrin-Lin <darrin.de88@gmail.com>
2023-11-27 12:38:58 +0800

    fix hw0405

--- test_compare/compare04/compare5.sh
+++ test_compare/compare04/compare5.sh
@@ -9,12 +9,19 @@
 # read directory
 # echo "file name:"
 # read filename
-
+cp ../../Homework4/HW04/hw0405.c .
+gcc hw0405.c -o ./05/hw0405
 for i in $(seq 0 9); do
+    echo " ${i}--------------------"
     ./05/hw0405 <./hw0405/in/0${i}.in >./05/hw0405_0${i}.out
     diff ./05/hw0405_0${i}.out ./hw0405/out/0${i}.out
 done
+for i in $(seq 10 19); do
+    echo "${i}--------------------"
+    ./05/hw0405 <./hw0405/in/${i}.in >./05/hw0405_${i}.out
+    diff ./05/hw0405_${i}.out ./hw0405/out/${i}.out
 
+done
 
 # for i in $(seq 0 9); do
 #     diff ./0${n}/out/hw0${hw}0${n}_0${i}.out ./${directory}/${filename}0${hw}0${n}_0${i}.out
