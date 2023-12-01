3d922915e68e19253439fbd8d5338bb43a3188fb
Darrin-Lin <darrin.de88@gmail.com>
2023-12-01 16:42:50 +0800

    create hw05

---
+++ Homework5/test_hw05/test0503.py
@@ -0,0 +1,7 @@
+import random as r
+hw=5
+hw_part=3
+for i in range(0,10):
+    with open('./0'+str(hw_part)+'/in/hw0'+str(hw)+'0'+str(hw_part)+"_0"+str(i)+'.in', 'w') as f:
+        f.write("")
+        f.write('\n')
