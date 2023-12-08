bcf9b599db2bb43a06028144471af05496f78c13
Darrin-Lin <darrin.de88@gmail.com>
2023-12-08 13:41:53 +0800

    update hw0503

--- Homework5/HW05/mysphere.c
+++ Homework5/HW05/mysphere.c
@@ -20,25 +20,25 @@ int32_t get_cap_area(double r, double a, double b, double c, double d, double *p
     double distance = 0;
     if (a == b && b == c && c == 0)
     {
-        return 0;
+        return -1;
     }
     // distance = fabs(a * 0 + b * 0 + c * 0 - d) / sqrt(a * a + b * b + c * c);
     distance = fabs(d) / sqrt(a * a + b * b + c * c);
-    if (distance == r)
+    if (fabs(distance - r) < 1e-6)
     {
         *pArea = 0;
         return 1;
     }
     else if (distance > r)
     {
-        *pArea = 0;
-        return 1;
+        // *pArea = 0;
+        return 0;
     }
     else
     {
         double area = 0;
         area = M_PI * (r * r - distance * distance);
-        area = (int)(area * 100 + 0.5) / 100.0;
+        // area = (int)(area * 100 + 0.5) / 100.0;
         *pArea = area;
         return 1;
     }
